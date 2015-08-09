c7747e40581e74f80859f5271fc8bd0b5b053cee
KBPratap <kbpratap@gmail.com>
2015-08-08 23:37:39 -0700

    initial project version

---
+++ sampleproject_y/Main.view.js
@@ -0,0 +1,23 @@
+sap.ui.jsview('sampleproject_y.Main', {
+
+    /**
+    * Specifies the Controller belonging to this View.
+    * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+    * @memberOf sampleproject_y.Main **/
+    getControllerName : function() {
+        return 'sampleproject_y.Main';
+    },
+
+    /** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed.
+    * Since the Controller is given to this method, its event handlers can be attached right away.
+    * @memberOf sampleproject_y.Main **/
+    createContent : function(oController) {
+        return new sap.m.Page({
+            title: 'Title',
+            content: [
+
+            ]
+        });
+    }
+
+});
